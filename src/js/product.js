192d3ca51788d1b0ebf006a107b4025255c16ca5
nguyenchinh98 <nguyenchinhutck57@gmail.com>
2022-10-10 18:03:44 +0700

    abc

--- src/js/product.js
+++ src/js/product.js
@@ -1,5 +1,5 @@
-import "../css/global.css";
-import "../css/product.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+import "../css/global.css";
+import "../css/product.css";
