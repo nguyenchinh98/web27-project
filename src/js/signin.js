3eea61e964e67f676bcd0add2fb35af9f8a61f07
nguyenchinh98 <nguyenchinhutck57@gmail.com>
2022-10-10 18:19:28 +0700

    up-project

--- src/js/signin.js
+++ src/js/signin.js
@@ -2,4 +2,4 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.js";
 import "../css/global.css";
-import "../css/signin.css"
+import "../css/signin.css";
