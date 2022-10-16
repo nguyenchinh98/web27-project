309324bfad82c057e40aeaf71a94e7dcea623f65
nguyenchinh98 <nguyenchinhutck57@gmail.com>
2022-10-17 02:49:55 +0700

    update

--- src/js/list_product.js
+++ src/js/list_product.js
@@ -3,4 +3,5 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "../css/global.css";
 import "../css/list_product.css";
+
 import "./backtop.js";
--- src/js/signup.js
+++ src/js/signup.js
@@ -3,3 +3,22 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.js";
 import "../css/global.css";
 import "../css/signup.css";
+
+
+
+function signup(e){
+    event.preventDefault();
+    let username = document.getElementById("username").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
+    let password = document.getElementById("password").value;
+    let user = {
+        username: username,
+        email: email,
+        phone: phone,
+        password: password,
+    }
+    let json = JSON.stringify(user);
+    localStorage.setItem(username, json)
+    alert("Đăng ký thành công")
+}
