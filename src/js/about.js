4938fe06a3bab98f4f323112ab2674e34605945e
nguyenchinh98 <nguyenchinhutck57@gmail.com>
2022-10-19 16:34:39 +0700

    abc

---
+++ src/js/about.js
@@ -0,0 +1,5 @@
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.js";
+import "../css/global.css";
+import "../css/about.css";
